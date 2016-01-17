34c83bfa074ec9715a2546b16ada2d51f2409158
Neil <cyberlionsystem@ymail.com>
2016-01-17 21:13:20 +0400

    Update RoShamBo.py
    Changed input from letters to numbers.

--- RoShamBo.py
+++ RoShamBo.py
@@ -1,5 +1,3 @@
-#Engineered by Akash Neil
-
 import random
 
 lose = ("You lose!")
@@ -11,55 +9,53 @@ score = 0
 computer_lives = 5
 
 while True:
-    print("Enter R for rock; P for paper and S for scissors!")
+    print("-------------------------------------------------")
+    print("Enter 1 for rock, 2 for paper and 3 for scissors! Enter 0 to exit the game!")
+    player_choice = input()
+    computer_choice = (1, 2, 3)
+    computer_choice = random.choice(computer_choice)
 
-    while True:
+    if player_choice == 1 and computer_choice == 2:
+        print("You choose rock, computer chooses paper!")
+        print (lose)
+        player_lives -= 1
 
-        player_choice = input()
-        computer_choice = ("R", "P", "S")
-        computer_choice = random.choice(computer_choice)
+    if player_choice == 3 and computer_choice == 1:
+        print("You choose scissors, computer chooses rock!")
+        print (lose)
+        player_lives -= 1
 
-        if player_choice == "R" and computer_choice == "P":
-            print("Computer chooses",computer_choice)
-            print (lose)
-            player_lives -= 1
+    if player_choice == 2 and computer_choice == 3:
+        print("You choose paper, computer chooses scissors!")
+        print (lose)
+        player_lives -= 1
 
-        if player_choice == "S" and computer_choice == "R":
-            print("Computer chooses",computer_choice)
-            print (lose)
-            player_lives -= 1
+    if player_choice == 2 and computer_choice == 1:
+        print("You choose paper, computer chooses rock!")
+        print (win)
+        computer_lives -= 1
 
-        if player_choice == "P" and computer_choice == "S":
-            print("Computer chooses",computer_choice)
-            print (lose)
-            player_lives -= 1
+    if player_choice == 1 and computer_choice == 3:
+        print("You choose rock, computer chooses scissors!")
+        print (win)
+        computer_lives -= 1
 
-        if player_choice == "P" and computer_choice == "R":
-            print("Computer chooses",computer_choice)
-            print (win)
-            computer_lives -= 1
+    if player_choice == 3 and computer_choice == 2:
+        print("You choose scissors, computer chooses paper!")
+        print (win)
+        computer_lives -= 1
 
-        if player_choice == "R" and computer_choice == "S":
-            print("Computer chooses",computer_choice)
-            print (win)
-            computer_lives -= 1
+    if player_choice == computer_choice:
+        print("You both choose the same thing!")
+        print (draw)
 
-        if player_choice == "S" and computer_choice == "P":
-            print("Computer chooses",computer_choice)
-            print (win)
-            computer_lives -= 1
+    if ((player_choice or computer_choice)!= 1) and ((player_choice or computer_choice)!= 2) and ((player_choice or computer_choice)!= 3):
+        print("Enter a valid command!")
 
-        if player_choice == computer_choice:
-            print("Computer chooses",computer_choice)
-            print (draw)
+    if player_lives ==0:
+        print ('You win the game!')
+        break
 
-        if ((player_choice or computer_choice)!= "R") and ((player_choice or computer_choice)!= "P") and ((player_choice or computer_choice)!= "S"):
-            print("Enter a valid command!")
-
-        if player_lives ==0:
-            print ('You win the game!')
-            break
-
-        if computer_lives == 0:
-            print ('Computer wins the game!')
-            break
+    if computer_lives == 0:
+        print ('Computer wins the game!')
+        break

1f896ccacfc1ce2f33f02c8f04a4e6f9a117965c
Neil <cyberlionsystem@ymail.com>
2016-01-17 16:47:04 +0400

    Create RoShamBo.py
    This file contains several bugs that need to be fixed.

---
+++ RoShamBo.py
@@ -0,0 +1,63 @@
+import random
+
+lose = ("You lose!")
+win = ("You win!")
+draw = ("Draw!")
+
+player_lives = 5
+score = 0
+computer_lives = 5
+
+while True:
+    print("Enter R for rock; P for paper and S for scissors!")
+
+    while True:
+
+        player_choice = input()
+        computer_choice = ("R", "P", "S")
+        computer_choice = random.choice(computer_choice)
+
+        if player_choice == "R" and computer_choice == "P":
+            print("Computer chooses",computer_choice)
+            print (lose)
+            player_lives -= 1
+
+        if player_choice == "S" and computer_choice == "R":
+            print("Computer chooses",computer_choice)
+            print (lose)
+            player_lives -= 1
+
+        if player_choice == "P" and computer_choice == "S":
+            print("Computer chooses",computer_choice)
+            print (lose)
+            player_lives -= 1
+
+        if player_choice == "P" and computer_choice == "R":
+            print("Computer chooses",computer_choice)
+            print (win)
+            computer_lives -= 1
+
+        if player_choice == "R" and computer_choice == "S":
+            print("Computer chooses",computer_choice)
+            print (win)
+            computer_lives -= 1
+
+        if player_choice == "S" and computer_choice == "P":
+            print("Computer chooses",computer_choice)
+            print (win)
+            computer_lives -= 1
+
+        if player_choice == computer_choice:
+            print("Computer chooses",computer_choice)
+            print (draw)
+
+        if ((player_choice or computer_choice)!= "R") and ((player_choice or computer_choice)!= "P") and ((player_choice or computer_choice)!= "S"):
+            print("Enter a valid command!")
+
+        if player_lives ==0:
+            print ('You win the game!')
+            break
+
+        if computer_lives == 0:
+            print ('Computer wins the game!')
+            break
